25332e2489bca77cabcfe30b949626b1845cd999
BobbyErickson89 <ericksonr07@gmail.com>
2024-10-23 22:26:29 -0400

    fixed blank space in excel file and formatting the date

--- src/utilities.ts
+++ src/utilities.ts
@@ -9,10 +9,14 @@ export const getCsvFile = async (filePath: string) => {
     }
 
     const csvString = await response.text();
-    const jsonData = Papa.parse(csvString, {
+    let jsonData = Papa.parse(csvString, {
       header: true,
       transformHeader: (header) => convertToCamelCase(header),
     }).data;
+
+    jsonData = jsonData.map((data: any) => {
+      return { ...data, birthday: formatDate(data.birthday) };
+    });
 
     return jsonData;
   } catch (error) {
@@ -31,4 +35,23 @@ export const convertToCamelCase = (str: string) => {
       return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
     })
     .join("");
+};
+
+export const formatDate = (dateStr: string): string => {
+  const longFormat = new Date(dateStr);
+  if (!isNaN(longFormat.getTime())) {
+    const month = longFormat.getMonth() + 1; // getMonth() returns 0-based month
+    const day = String(longFormat.getDate()).padStart(2, "0");
+    const year = longFormat.getFullYear();
+    return `${month}/${day}/${year}`;
+  }
+
+  const [month, day, year] = dateStr.split("/");
+  return `${parseInt(month, 10)}/${String(day).padStart(2, "0")}/${year}`;
+};
+
+// TODO: could be used for checking if date is already in correct format
+export const isDateInMDYFormat = (dateStr: string): boolean => {
+  const regex = /^(0?[1-9]|1[0-2])\/(0?[1-9]|[12][0-9]|3[01])\/\d{4}$/;
+  return regex.test(dateStr);
 };
