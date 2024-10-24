c0999ab3b40b499290780a214d8364515916af4a
BobbyErickson89 <ericksonr07@gmail.com>
2024-10-23 21:57:01 -0400

    tried material ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,21 @@ import logo from "./logo.svg";
 import { getCsvFile } from "./utilities";
 import "./App.css";
 import { CsvFileJson } from "./types";
+import {
+  AppBar,
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
-function App() {
+export default function App() {
   const [csvData, setCsvData] = useState<CsvFileJson[]>([]);
+  const [monthFilter, setMonthFilter] = useState<string>("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,26 +29,57 @@ function App() {
     fetchData();
   }, []);
 
-  console.log("csv data:", csvData);
+  // const useStyles = makeStyles({
+  //   select: {
+  //     "&:before": {
+  //       borderColor: "white",
+  //     },
+  //     "&:after": {
+  //       borderColor: "white",
+  //     },
+  //     "&:not(.Mui-disabled):hover::before": {
+  //       borderColor: "white",
+  //     },
+  //   },
+  //   icon: {
+  //     fill: "white",
+  //   },
+  //   root: {
+  //     color: "white",
+  //   },
+  // });
+
+  // const classes = useStyles();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={headerHeight}>
+        <Toolbar>
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+            Employee Information
+          </Typography>
+          <Button variant="outlined" color="inherit">
+            Apply Filter
+          </Button>
+          <FormControl size="small">
+            <InputLabel id="month-filter">Month</InputLabel>
+            <Select
+              labelId="month-filter"
+              value={monthFilter}
+              label="Month"
+              onChange={(e) => setMonthFilter(e.target.value)}
+              sx={{ width: "200px", color: "white" }}
+            >
+              <MenuItem value="January">January</MenuItem>
+            </Select>
+          </FormControl>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
-export default App;
+const headerHeight = {
+  minHeight: "100px",
+  justifyContent: "center",
+};
